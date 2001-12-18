3b45017b44b5def403c0862d1ed4f6f26def02aa
Roar Thronæs <roart@nvg.ntnu.no>
2001-12-18 12:49:46 +0000

    interrupts worked just with one boot

--- sys/src/sched.c
+++ sys/src/sched.c
@@ -643,8 +643,8 @@ int mycount=0;
 
 asmlinkage void schedule(void) {
   //  SOFTINT_RESCHED_VECTOR;
-__asm__ __volatile__ ("int $0x93\n");
-//  sch$resched();
+  //__asm__ __volatile__ ("int $0x93\n");
+  sch$resched();
 }
 
 asmlinkage void sch$sched(int from_sch$resched) {
