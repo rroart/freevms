6ba661d002a78712132e9a13c914c85a0ea24aeb
Roar Thronæs <roart@nvg.ntnu.no>
2002-02-13 17:12:12 +0000

    just a check-in. no new integrated functionality, just some partially finished stuff

---
+++ lib/src/lkbdef.h
@@ -0,0 +1,92 @@
+#ifndef lkbdef_h
+#define lkbdef_h
+
+#define LKB$M_FLAGS_VALID 0x4
+#define LKB$M_PKAST 0x10
+#define LKB$M_NODELETE 0x20
+#define LKB$M_QUOTA 0x40
+#define LKB$M_KAST 0x80
+#define LKB$K_ACB64LEN 64
+#define LKB$M_DCPLAST 0x1
+#define LKB$M_ASYNC 0x4
+#define LKB$M_BLKASTQED 0x8
+#define LKB$M_MSTCPY 0x10
+#define LKB$M_NOQUOTA 0x20
+#define LKB$M_TIMOUTQ 0x40
+#define LKB$M_WASSYSOWN 0x80
+#define LKB$M_CVTTOSYS 0x100
+#define LKB$M_PROTECT 0x200
+#define LKB$M_RESEND 0x400
+#define LKB$M_RM_RBRQD 0x800
+#define LKB$M_FLOCK 0x1000
+#define LKB$M_IP 0x2000
+#define LKB$M_CACHED 0x4000
+#define LKB$M_RNGBLK 0x8000
+#define LKB$M_BRL 0x10000
+#define LKB$M_NEWSUBRNG 0x20000
+#define LKB$M_CVTSUBRNG 0x40000
+#define LKB$M_RNGCHG 0x80000
+#define LKB$M_2PC_IP 0x100000
+#define LKB$M_2PC_PEND 0x200000
+#define LKB$M_BLKASTFLG 0x400000
+#define LKB$M_GRSUBRNG 0x800000
+#define LKB$M_PCACHED 0x1000000
+#define LKB$M_VALBLKRD 0x2000000
+#define LKB$M_VALBLKWRT 0x4000000
+#define LKB$M_DPC 0x8000000
+#define LKB$M_PERCPU 0x10000000
+#define LKB$M_INDEX 0xFFFFFF
+#define LKB$K_GRANTED 1
+#define LKB$K_CONVERT 0
+#define LKB$K_WAITING -1
+#define LKB$K_RETRY -2
+#define LKB$K_SCSWAIT -3
+#define LKB$K_RSPNOTQED -4
+#define LKB$K_RSPQUEUED -5
+#define LKB$K_RSPGRANTD -6
+#define LKB$K_RSPDOLOCL -7
+#define LKB$K_RSPRESEND -8
+#define LKB$K_RSPWAIT -9
+#define LKB$K_RSP2PCRDY -10
+#define LKB$K_RSPFATAL -11
+#define LKB$M_DBLKAST 0x2
+#define LKB$K_LENGTH 248
+#define LKB$C_LENGTH 248
+#define LKB$S_LKBDEF 248
+	
+struct _lkb$r_lkb {
+  struct _acb *lkb$l_astqfl;
+  struct _acb *lkb$l_astqbl;
+  unsigned short int lkb$w_size;
+  unsigned char lkb$b_type;
+  unsigned char lkb$b_rmod;
+  unsigned int lkb$l_pid;
+  unsigned int lkb$l_ast;
+  unsigned int lkb$l_astprm;
+  unsigned int lkb$l_kast;
+  unsigned long lkb$l_cplastadr;
+  unsigned long lkb$l_blkastadr;
+  unsigned int lkb$l_flags;
+  unsigned int lkb$l_status;
+  unsigned int lkb$l_lkst1;
+  unsigned int lkb$l_lkst2;
+  unsigned char lkb$b_rqmode;
+  unsigned char lkb$b_grmode;
+  unsigned char lkb$b_state;
+  unsigned char lkb$b_efn;
+  void *lkb$q_sqfl;
+  void *lkb$q_sqbl;
+  void *lkb$q_ownqfl;
+  void *lkb$q_ownqbl;
+  struct _lkb *lkb$q_parent;
+  unsigned short lkb$w_refcnt;
+  unsigned char lkb$b_tslt;
+  unsigned char lib$b_res;
+  struct _rsb *lkb$q_rsb;
+  unsigned int lkb$l_remlkid;
+  unsigned int lkb$l_csid;
+  int (*lkb$pq_oldblkast)();
+};
+ 
+#endif
+ 
---
+++ lib/src/rsbdef.h
@@ -0,0 +1,75 @@
+#ifndef rsbdef_h
+#define rsbdef_h
+
+#define RSB$M_DIRENTRY 0x1
+#define RSB$M_VALINVLD 0x2
+#define RSB$M_DIR_RQD 0x4
+#define RSB$M_RM_PEND 0x8
+#define RSB$M_RM_IP 0x10
+#define RSB$M_RM_ACCEPT 0x20
+#define RSB$M_RM_RBLD 0x40
+#define RSB$M_RM_WAIT 0x80
+#define RSB$M_RM_DEFLECT 0x100
+#define RSB$M_DIR_IP 0x200
+#define RSB$M_RBLD_IP 0x400
+#define RSB$M_RBLD_RQD 0x800
+#define RSB$M_RBLD_ACT 0x1000
+#define RSB$M_CHK_BTR 0x2000
+#define RSB$M_WTFULRNG 0x4000
+#define RSB$M_WTSUBRNG 0x8000
+#define RSB$M_BRL 0x10000
+#define RSB$M_2PC_IP 0x80000
+#define RSB$M_CVTFULRNG 0x100000
+#define RSB$M_CVTSUBRNG 0x200000
+#define RSB$M_VALCUR 0x400000
+#define RSB$M_INVPEND 0x800000
+#define RSB$M_DPC 0x1000000
+#define RSB$M_GGMODE_VALID 0x2000000
+#define RSB$K_MAXLEN 31
+#define RSB$K_LENGTH 256
+#define RSB$C_LENGTH 256
+#define RSB$S_RSBDEF 256
+	
+struct _rsb {
+  void *rsb$l_hshchn;
+  void *rsb$l_hshchnbk;
+
+  unsigned short int rsb$w_size;
+  unsigned char rsb$b_type;
+  unsigned char rsb$b_fgmode;
+  unsigned char rsb$b_ggmode;
+  unsigned char rsb$b_cgmode;
+  unsigned int rsb$l_status;
+  unsigned int rsb$l_refcnt;
+  unsigned int rsb$l_csid;
+  unsigned int rsb$l_rm_csid;
+  void *rsb$l_grqfl;
+  void *rsb$l_grqbl;
+  void *rsb$l_cvtqfl;
+  void *rsb$l_cvtqbl;
+  void *rsb$l_wtqfl;
+  void *rsb$l_wtqbl;
+  unsigned long long rsb$q_valblk;
+  unsigned long rsb$l_csid;
+  void *rsb$l_rrsfl;
+  void *rsb$l_rrsbl;
+  void *rsb$l_srsfl;
+  void *rsb$l_srsbl;
+  unsigned long rsb$l_rm_csid;
+  struct _rsb *rsb$l_rtrsb;
+  struct _clurcb *rsb$l_clurcb;
+  unsigned short int rsb$w_activity;
+  unsigned short int rsb$w_lckcnt;
+  unsigned int rsb$l_valseqnum;
+  unsigned short int rsb$w_refcnt;
+  unsigned short int rsb$w_blkastcnt;
+  unsigned short int rsb$w_hashval;
+  unsigned short int rsb$w_rqseqnm;
+  struct _rsb *rsb$l_parent;
+  unsigned short int rsb$w_group;
+  unsigned char rsb$b_rmod;
+  unsigned char rsb$b_rsnlen;
+  char rsb$t_resnam [32];
+};
+ 
+#endif
