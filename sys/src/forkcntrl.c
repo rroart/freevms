b81364057690b27e1ee128ca736c3f365d638021
Roar Thronæs <roart@nvg.ntnu.no>
2002-05-16 12:57:15 +0000

    normal amount of fork interrupts

--- sys/src/forkcntrl.c
+++ sys/src/forkcntrl.c
@@ -10,15 +10,35 @@
 #include <linux/kernel.h>
 #include <asm/hw_irq.h>
 
-asmlinkage void exe$forkdspth(void) {
+asmlinkage void exe$frkipl6dsp(void) {
+  exe$forkdspth(6);
+}
+
+asmlinkage void exe$frkipl8dsp(void) {
+  exe$forkdspth(8);
+}
+
+asmlinkage void exe$frkipl9dsp(void) {
+  exe$forkdspth(9);
+}
+
+asmlinkage void exe$frkipl10dsp(void) {
+  exe$forkdspth(10);
+}
+
+asmlinkage void exe$frkipl11dsp(void) {
+  exe$forkdspth(11);
+}
+
+asmlinkage void exe$forkdspth(int i) {
   void (*func)(void *,void *);
   struct _fkb * f, * dummy, * fq;
-  if (intr_blocked(IPL$_QUEUEAST))
+  if (intr_blocked(i))
     return;
-  regtrap(REG_INTR, IPL$_QUEUEAST);
-  setipl(IPL$_TIMER);
+  regtrap(REG_INTR, i);
+  setipl(i);
   printk("forkdspth\n");
-  fq=smp$gl_cpu_data[smp_processor_id()]->cpu$q_swiqfl[0]; /* so far */
+  fq=smp$gl_cpu_data[smp_processor_id()]->cpu$q_swiqfl[i-6]; /* so far */
   while (!aqempty(fq)) {
     f=remque(fq,dummy);
     printk("forking entry %x\n",f);
@@ -44,6 +64,24 @@ void exe$iofork(struct _irp * i, struct _ucb * u) {
   insque(u,f->fkb$l_fqbl);
   if (isempty) {
     /* do SOFTINTS */
-    SOFTINT_QUEUEAST_VECTOR; /* wrong one, but I did not want to recompile because of changing a central .h */
+    switch (newipl) {
+    case IPL$_IOLOCK11:
+      SOFTINT_IOLOCK11_VECTOR;
+      break;
+    case IPL$_IOLOCK10:
+      SOFTINT_IOLOCK10_VECTOR;
+      break;
+    case IPL$_IOLOCK9:
+      SOFTINT_IOLOCK9_VECTOR;
+      break;
+    case IPL$_IOLOCK8:
+      SOFTINT_IOLOCK8_VECTOR;
+      break;
+    case IPL$_QUEUEAST:
+      SOFTINT_QUEUEAST_VECTOR;
+      break;
+    default:
+      panic("in forkdspth\n");
+    }
   }
 }
