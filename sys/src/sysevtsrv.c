bfc2e8df6951976bcdcd82eb36e83c0cd721785f
Roar Thronæs <roart@nvg.ntnu.no>
2002-06-16 11:00:49 +0000

    a little better + plan?

--- sys/src/sysevtsrv.c
+++ sys/src/sysevtsrv.c
@@ -7,11 +7,18 @@
 #include "../../freevms/sys/src/system_data_cells.h"
 
 asmlinkage int exe$clref(unsigned int efn) {
+  int retval;
   struct _pcb * p=smp$gl_cpu_data[smp_processor_id()]->cpu$l_curpcb;
   int efncluster=(efn&224)>>5;
   unsigned long * clusteraddr;
   clusteraddr=&p->pcb$l_efcs+efncluster;
+  retval=(*clusteraddr)&(1<<(efn&31));
+  /* do a bbcci instead */
   (*clusteraddr)&=~(1<<(efn&31));
+  if (retval)
+    return SS$_WASSET;
+  else
+    return SS$_WASCLR;
 }
 
 asmlinkage int exe$readef(unsigned int efn, unsigned int *state) {
