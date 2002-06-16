4f1b9f2e0a24e49670db9a8d6a624a23a5d498b6
Roar Thronæs <roart@nvg.ntnu.no>
2002-06-16 11:03:09 +0000

    bbssi

--- sys/src/postef.c
+++ sys/src/postef.c
@@ -1,4 +1,5 @@
 #include<linux/sched.h>
+#include<asm/bitops.h>
 #include"../../freevms/starlet/src/ssdef.h"
 #include"../../freevms/lib/src/evtdef.h"
 #include"../../freevms/lib/src/ipldef.h"
@@ -26,11 +27,18 @@ int sch$postef(unsigned long ipid, unsigned long priclass, unsigned long efn) {
   p=find_process_by_pid(ipid);
   if (!p) return SS$_NONEXPR;
   clusteraddr=&p->pcb$l_efcs+efncluster;
+#if 0
+  // replacing with bbssi
   if ((*clusteraddr)&(~(1<<(efn&31))))
     retval=SS$_WASSET;
   else
     retval=SS$_WASCLR;
   (*clusteraddr)|=(1<<(efn&31));
+#endif
+  if (test_and_set_bit(efn&31,clusteraddr)) // bbssi
+    retval=SS$_WASSET;
+  else
+    retval=SS$_WASCLR;
   waitcheck(p,priclass,&p->pcb$l_efcs+4*p->pcb$b_wefc,clusteraddr);
   vmsunlock(&SPIN_SCHED,savipl);
   return retval;
