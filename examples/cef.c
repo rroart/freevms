d9072c04cc9171615b628f7695392369ca5af476
Roar Thronæs <roart@nvg.ntnu.no>
2002-06-20 10:04:14 +0000

    cef examples

--- examples/cef.c
+++ examples/cef.c
@@ -7,30 +7,36 @@ signed long long step1;
 
 void my1(int i) {
 printf("here1\n");
-sys$setef(63);
+sys$setef(126);
 }
 
 main(){
 int flag ;
 
-sys$readef(31,&flag);
+$DESCRIPTOR(NAME1,"numberone");
+$DESCRIPTOR(NAME2,"numbertwo");
+
+sys$ascefc(66,&NAME1);
+sys$ascefc(99,&NAME2);
+
+sys$readef(93,&flag);
 printf("flag %x\n",flag);
-sys$readef(63,&flag);
+sys$readef(126,&flag);
 printf("flag %x\n",flag);
 
 sys$setef(51);
-sys$setef(20);
+sys$setef(80);
 
-sys$readef(31,&flag);
+sys$readef(93,&flag);
 printf("flag %x\n",flag);
-sys$readef(51,&flag);
+sys$readef(111,&flag);
 printf("flag %x\n",flag);
 
-sys$clref(51);
+sys$clref(111);
 
-sys$readef(31,&flag);
+sys$readef(93,&flag);
 printf("flag %x\n",flag);
-sys$readef(63,&flag);
+sys$readef(126,&flag);
 printf("flag %x\n",flag);
 
 
