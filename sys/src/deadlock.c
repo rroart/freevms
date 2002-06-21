d62a14becd7412b7ce6aa48533a0652a648a4d72
Roar Thronæs <roart@nvg.ntnu.no>
2002-06-21 13:17:39 +0000

    new

---
+++ sys/src/deadlock.c
@@ -0,0 +1,21 @@
+// $Id$
+// $Locker$
+
+// Author. Roar Thronæs.
+
+#include<linux/linkage.h>
+#include<linux/vmalloc.h>
+#include<asm/current.h>
+#include"../../freevms/sys/src/system_data_cells.h"
+#include"../../freevms/starlet/src/lckdef.h"
+#include"../../freevms/lib/src/lckctxdef.h"
+#include"../../freevms/starlet/src/misc.h"
+#include"../../freevms/starlet/src/ssdef.h"
+#include"../../freevms/starlet/src/starlet.h"
+#include"../../freevms/lib/src/rsbdef.h"
+#include"../../freevms/lib/src/lkbdef.h"
+#include"../../freevms/librtl/src/descrip.h"
+
+void lck$searchdlck(void) {
+
+}
