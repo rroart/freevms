4b831a6ac6e2973c3a8cc1acf539627572f9f416
Roar Thronæs <roart@nvg.ntnu.no>
2002-08-17 11:11:29 +0000

    quiet and with new routine ioc_bufpost

--- sys/src/iociopost.c
+++ sys/src/iociopost.c
@@ -21,7 +21,7 @@ dirpost(struct _irp * i) {
 
 bufpost(struct _irp * i) {
   struct _acb * a=(struct _acb *) i;
-  printk("doing bufpost\n");
+  //printk("doing bufpost\n");
   /* do iosb soon? */
   if (i->irp$l_iosb) {
     bcopy(&i->irp$l_iost1,i->irp$l_iosb,8);
@@ -46,7 +46,7 @@ asmlinkage void ioc$iopost(void) {
 
   setipl(IPL$_IOPOST);
 
-  printk("iopost %x %x %x %x\n",&ioc$gq_postiq,ioc$gq_postiq,current->pid,ioc$gq_postiq>>32); //,&ioc$gq_postiq,ioc$gq_postiq &ioc$gq_postiq,ioc$gq_postiq);
+  //printk("iopost %x %x %x %x\n",&ioc$gq_postiq,ioc$gq_postiq,current->pid,ioc$gq_postiq>>32); //,&ioc$gq_postiq,ioc$gq_postiq &ioc$gq_postiq,ioc$gq_postiq);
  again:
   if (!rqempty(&ioc$gq_postiq)) {
     i=remqhi(&ioc$gq_postiq,i);
@@ -79,14 +79,23 @@ asmlinkage void ioc$iopost(void) {
   goto again;
 
  bufio:
-
   i->irp$b_rmod|=ACB$M_KAST;
+
+  // put ioc$bufpost here?
   ((struct _acb *) i)->acb$l_kast=bufpost;
   ((struct _acb *) i)->acb$l_astprm=i;
   /* find other class than 1 */
   sch$postef(p->pid,PRI$_IOCOM,i->irp$b_efn);
   sch$qast(p->pid,PRI$_IOCOM,i);
   goto again;
+
+  ioc$bufpost();
+}
+
+ioc$bufpost(struct _irp * i){
+  i->irp$b_rmod|=ACB$M_KAST; // think this belongs here too
+  i->irp$l_wind=bufpost; // really acb$l_kast;
+  i->irp$l_astprm=i; // think this belongs here too
 }
 
 void ioc$myiopost(struct _pcb * p,unsigned long priclass) {
