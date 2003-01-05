8e52ba34d530485320e0d153ad891ca047ef1345
Roar Thronæs <roart@nvg.ntnu.no>
2003-01-05 14:12:28 +0000

    new

---
+++ sys/src/vmspte.h
@@ -0,0 +1,79 @@
+#ifndef vmspte_h
+#define vmspte_h
+
+// $Id$
+// $Locker$
+
+// Author. Roar Thronæs.
+
+// This will replace lib/src/ptedef.h, which is for Alpha
+// Later, this will replace include/asm/pgtable.h
+
+//FreeVMS PTE additions
+#define _PAGE_BIT_TYP1 11
+#define _PAGE_BIT_TYP0 10
+#define _PAGE_BIT_TYP 9
+#define _PAGE_TYP1 0x800
+#define PTE$M_TYP1 0x800
+#define _PAGE_TYP0 0x400
+#define PTE$M_TYP0 0x400
+//#define _PAGE_TYP 0x200
+
+
+#define PTE$M_CRF 0x1000
+
+struct _mypte {
+  union {
+    struct {
+      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned yet_unused : 1;
+      unsigned pte$v_typ0 : 1;
+      unsigned pte$v_typ1 : 1;
+      unsigned pte$v_stx : 16;
+      unsigned pte$v_crf : 1; 
+      unsigned pte$v_dzro : 1; 
+      unsigned pte$v_wrt : 1;
+      unsigned pte$v_fill : 1;
+    };
+    struct {
+      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned yet_unused : 1;
+      unsigned pte$v_typ0 : 1;
+      unsigned pte$v_typ1 : 1;
+      unsigned pte$v_pgflpag : 18;
+      unsigned pte$v_pgflx : 2;
+    };
+    struct {
+      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned yet_unused : 1;
+      unsigned pte$v_typ0 : 1;
+      unsigned pte$v_typ1 : 1;
+      unsigned pte$v_gptx : 20;
+    };
+    struct {
+      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned yet_unused : 1;
+      unsigned pte$v_typ0 : 1;
+      unsigned pte$v_typ1 : 1;
+      unsigned pte$v_pfn : 20;
+    };
+    struct {
+      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned pte$v_window : 1;
+      unsigned pte$v_typ0 : 1;
+      unsigned pte$v_typ1 : 1;
+      unsigned pte$v_pfn : 20;
+    };
+    struct {
+      unsigned pte$v_valid : 1;
+      unsigned nothing : 6;
+      unsigned pte$v_global : 1;
+      unsigned pte$v_gblwrt : 1;
+      unsigned pte$v_fill : 23;
+    };
+    unsigned long pte$l_all;
+  };
+};
+
+#endif
+
