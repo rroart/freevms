2aaa1a8e65d06beb748c911fa557b13fbe66420d
Roar Thronæs <roart@nvg.ntnu.no>
2003-01-23 17:05:32 +0000

    new getjpi and ipid/epid support

--- sys/src/sysgetjpi.c
+++ sys/src/sysgetjpi.c
@@ -4,7 +4,43 @@
 // Author. Roar Thronæs.
 
 #include<linux/linkage.h>
+#include<linux/sched.h>
 
-asmlinkage void exe$getjpi(void) {
+#include<starlet.h>
+#include<jpidef.h>
+#include <ssdef.h>
+#include <misc.h>
 
+// this behaves like getjpiw for now
+
+asmlinkage int exe$getjpi(unsigned int efn, unsigned int *pidadr, void * prcnam, void *itmlst, struct _iosb *iosb, void (*astadr)(), unsigned long long astprm) {
+  struct _pcb * p;
+  int sts;
+  struct item_list_3 * it=itmlst;
+  sts=exe$pscan_next_id(&p);
+  if (sts==0)
+    return SS$_NOMOREPROC;
+  while (it->item_code) {
+    switch (it->item_code) {
+    case JPI$_PRCNAM:
+      bcopy(&p->pcb$t_lname,it->bufaddr,15);
+      break;
+
+    case JPI$_PID:
+      bcopy(&p->pcb$l_epid,it->bufaddr,4);
+      break;
+
+    case JPI$_MASTER_PID:
+      bcopy(&p->pid,it->bufaddr,4);
+      break;
+
+    }
+    it++;
+  }
+  return 1;
 }
+
+asmlinkage int exe$getjpi_wrap(struct struct_getjpi *s) {
+  return exe$getjpi(s->efn,s->pidadr,s->prcnam,s->itmlst,s->iosb,s->astadr,s->astprm);
+}
+
