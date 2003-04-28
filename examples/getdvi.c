db8ad1f59b19cf4678ecb35e9901c6ddc34bc35c
Roar Thronæs <roart@nvg.ntnu.no>
2003-04-28 08:03:43 +0000

    getdvi device_scan soon

---
+++ examples/getdvi.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <ssdef.h> 
+#include <descrip.h> 
+#include <dvidef.h>
+#include <starlet.h>
+
+main(){
+int i;
+struct item_list_3 lst[4];
+char devname[15];
+char devlen;
+int dvistatus;
+
+lst[0].buflen=15;
+lst[0].item_code=DVI$_DEVNAM;
+lst[0].bufaddr=devname;
+lst[0].retlenaddr=&devlen;
+lst[1].buflen=0;
+lst[1].item_code=0;
+do {
+dvistatus=sys$device_scan(0,0,0,lst,0,0,0);
+printf("%s\n",devname);
+} while (jpistatus == SS$_NORMAL);
+//} while (jpistatus != SS$_NOMOREPROC);
+printf("jpistatus %x\n",dvistatus);
+}
