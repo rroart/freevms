e54b5808c60a15cf7d7a0b063aa34882fd74bd3f
Roar Thronæs <roart@nvg.ntnu.no>
2003-05-05 07:06:26 +0000

    getting a barely working getdvi

--- sys/src/sysgetdvi.c
+++ sys/src/sysgetdvi.c
@@ -16,7 +16,41 @@ asmlinkage int exe$getdvi_wrap(struct struct_getdvi *s) {
   return exe$getdvi(s->efn,s->chan,s->devnam,s->itmlst,s->iosb,s->astadr,s->astprm,s->nullarg);
 }
 
+struct _ddb * contxt=0;
+
 asmlinkage int exe$getdvi(unsigned int efn, unsigned short int chan, void *devnam, void *itmlst, struct _iosb *iosb, void (*astadr)(), int astprm, struct _generic_64 *nullarg) {
+
+  struct _ddb * d;
+  struct item_list_3 * it=itmlst;
+
+  exe$clref(efn);
+  // check and do with iosb
+  // check quota 
+  // check chan no?
+  // then check devnam
+
+
+  // check itemcode with dvsdef defs
+  if (contxt)
+    d=contxt;
+  else
+    d=ioc$gl_devlist;
+  // sch$iolockr
+  while (it->item_code) {
+    switch (it->item_code) {
+    case DVI$_DEVNAM:
+      bcopy(&d->ddb$t_name,it->bufaddr,15);
+      break;
+
+    }
+    it++;
+  }
+
+  contxt=d->ddb$l_link;
+  if (contxt==0) 
+    return SS$_NOMOREDEV;
+
+  return SS$_NORMAL;
 
 }
 
