d22019fecb90cfe4c4158be14129e926cbbac7aa
Roar Thronæs <roart@nvg.ntnu.no>
2003-05-19 08:25:34 +0000

    new

--- lan/src/8390.c
+++ lan/src/8390.c
@@ -1,3 +1,161 @@
+#include<crbdef.h>
+#include<cdtdef.h>
+#include<ddtdef.h>
+#include<dptdef.h>
+#include<fdtdef.h>
+#include<pdtdef.h>
+#include<idbdef.h>
+#include<irpdef.h>
+#include<ucbdef.h>
+#include<ddbdef.h>
+#include<ipldef.h>
+#include<dyndef.h>
+#include<ssdef.h>
+#include<iodef.h>
+#include<devdef.h>
+#include<system_data_cells.h>
+#include<ipl.h>
+#include<linux/vmalloc.h>
+#include<linux/pci.h>
+#include<system_service_setup.h>
+#include<descrip.h>
+
+#include<linux/skbuff.h>
+
+struct _ucb * erau;
+
+static unsigned long startio (struct _irp * i, struct _ucb * u)
+{
+  unsigned long sts=SS$_NORMAL;
+  struct request * rq = kmalloc (sizeof(struct request), GFP_KERNEL);
+  struct sk_buff skb = dev_alloc_skb(i->irp$l_qio_p2+2);
+  skb->len=i->irp$l_qio_p2;
+  skb->data=i->irp$l_qio_p1;
+  struct net_device dev=i->irp$l_qio_p4;
+
+  //printk("era startio %x %x\n",i->irp$l_qio_p1,i->irp$l_qio_p2);
+
+  erau=u;
+
+  switch (i->irp$v_fcode) {
+
+    case IO$_WRITEPBLK: {
+	  ei_start_xmit(skb,dev);
+      return (sts);
+    }
+
+    case IO$_READPBLK: {
+	  // not really ?
+      return (sts);
+    }
+
+    /* Who knows what */
+
+    default: {
+      return (SS$_BADPARAM);
+    }
+  }
+}
+
+static struct _irp * globali;
+static struct _ucb * globalu;
+
+static void  startio3 (struct _irp * i, struct _ucb * u) { 
+  ioc$reqcom(SS$_NORMAL,0,u);
+  return;
+};
+
+static void  startio2 (struct _irp * i, struct _ucb * u) { 
+  u->ucb$l_fpc=startio3;
+  exe$iofork(i,u);
+  return;
+}
+
+static void ubd_intr2(int irq, void *dev, struct pt_regs *unused)
+{
+  struct _irp * i;
+  struct _ucb * u;
+  void (*func)();
+
+  if (intr_blocked(20))
+    return;
+  regtrap(REG_INTR,20);
+  setipl(20);
+  /* have to do this until we get things more in order */
+  i=globali;
+  u=globalu;
+
+  func=u->ucb$l_fpc;
+  func(i,u);
+  myrei();
+}
+
+static struct _fdt fdt = {
+  fdt$q_valid:IO$_NOP|IO$_UNLOAD|IO$_AVAILABLE|IO$_PACKACK|IO$_SENSECHAR|IO$_SETCHAR|IO$_SENSEMODE|IO$_SETMODE|IO$_WRITECHECK|IO$_READPBLK|IO$_WRITELBLK|IO$_DSE|IO$_ACCESS|IO$_ACPCONTROL|IO$_CREATE|IO$_DEACCESS|IO$_DELETE|IO$_MODIFY|IO$_MOUNT|IO$_READRCT|IO$_CRESHAD|IO$_ADDSHAD|IO$_COPYSHAD|IO$_REMSHAD|IO$_SHADMV|IO$_DISPLAY|IO$_SETPRFPATH|IO$_FORMAT,
+  fdt$q_buffered:IO$_NOP|IO$_UNLOAD|IO$_AVAILABLE|IO$_PACKACK|IO$_DSE|IO$_SENSECHAR|IO$_SETCHAR|IO$_SENSEMODE|IO$_SETMODE|IO$_ACCESS|IO$_ACPCONTROL|IO$_CREATE|IO$_DEACCESS|IO$_DELETE|IO$_MODIFY|IO$_MOUNT|IO$_CRESHAD|IO$_ADDSHAD|IO$_COPYSHAD|IO$_REMSHAD|IO$_SHADMV|IO$_DISPLAY|IO$_FORMAT
+};
+
+/* more yet undefined dummies */
+//static void  startio ();
+static void  unsolint (void) { };
+static void  functb (void) { };
+static void  cancel (void) { };
+static void  regdump (void) { };
+static void  diagbuf (void) { };
+static void  errorbuf (void) { };
+static void  unitinit (void) { };
+static void  altstart (void) { };
+static void  mntver (void) { };
+static void  cloneducb (void) { };
+static void  mntv_sssc (void) { };
+static void  mntv_for (void) { };
+static void  mntv_sqd (void) { };
+static void  aux_storage (void) { };
+static void  aux_routine (void) { };
+
+static struct _ddt ddt = {
+  ddt$l_start: startio,
+  ddt$l_unsolint: unsolint,
+  ddt$l_functb: functb,
+  ddt$l_cancel: cancel,
+  ddt$l_regdump: regdump,
+  ddt$l_diagbuf: diagbuf,
+  ddt$l_errorbuf: errorbuf,
+  ddt$l_unitinit: unitinit,
+  ddt$l_altstart: altstart,
+  ddt$l_mntver: mntver,
+  ddt$l_cloneducb: cloneducb,
+  ddt$w_fdtsize: 0,
+  ddt$l_mntv_sssc: mntv_sssc,
+  ddt$l_mntv_for: mntv_for,
+  ddt$l_mntv_sqd: mntv_sqd,
+  ddt$l_aux_storage: aux_storage,
+  ddt$l_aux_routine: aux_routine
+};
+
+static struct _ddb ddb;
+static struct _dpt dpt;
+
+int acp_std$readblk();
+
+extern void ini_fdt_act(struct _fdt * f, unsigned long long mask, void * fn, unsigned long type);
+
+int ide_vmsinit(void) {
+  unsigned short chan;
+  $DESCRIPTOR(u0,"era0");
+  struct _ucb * u=makeucbetc(&ddb,&ddt,&dpt,&fdt,"era0","erdriver");
+  printk(KERN_INFO "dev here\n");
+  /* for the fdt init part */
+  /* a lot of these? */
+  ini_fdt_act(&fdt,IO$_READLBLK,acp_std$readblk,1);
+  ini_fdt_act(&fdt,IO$_READPBLK,acp_std$readblk,1);
+  ini_fdt_act(&fdt,IO$_READVBLK,acp_std$readblk,1);
+  exe$assign(&u0,&chan,0,0,0);
+  registerdevchan(MKDEV(IDE0_MAJOR,0),chan);
+  printk(KERN_INFO "dev here\n");
+  return chan;
+}
+
 /* 8390.c: A general NS8390 ethernet driver core for linux. */
 /*
 	Written 1992-94 by Donald Becker.
