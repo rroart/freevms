a2c2b44f3d5a4505a4692783b351806898f4c371
Roar Thronæs <roart@nvg.ntnu.no>
2003-06-03 12:44:15 +0000

    use nampid and do more properly.

--- sys/src/syssetpri.c
+++ sys/src/syssetpri.c
@@ -7,25 +7,32 @@
 #include<linux/linkage.h>
 #include<linux/sched.h>
 #include<starlet.h>
+#include<ssdef.h>
+#include<evtdef.h>
+#include<pridef.h>
 
 /* Author: Roar Thronæs */
 
-//asmlinkage int sys_$setpri(unsigned int *pidadr, void *prcnam, unsigned int pri, unsigned int *prvpri, unsigned int*pol, unsigned int *prvpol) {
-asmlinkage int exe$setpri(struct struct_setpri *s) {
+asmlinkage int exe$setpri_wrap(struct struct_setpri *s) {
+  return exe$setpri(s->pidadr,s->prcnam,s->pri,s->prvpri,s->pol,s->prvpol);
+}
+
+asmlinkage int exe$setpri(unsigned int *pidadr, void *prcnam, unsigned int pri, unsigned int *prvpri, unsigned int*pol, unsigned int *prvpol) {
   struct _pcb *p;
-  if (s->pidadr) {
-    p=current;
-  } else {
-    int i;
-    for (i=0;i<PIDHASH_SZ; i++)
-      if (pidhash[i]->pid==*(s->pidadr)) {
-	p=pidhash[i];
-	break;
-      }
-  }
+  struct _pcb * retpcb;
+  unsigned long ipid, epid;
+  int sts;
+  sts=exe$nampid(current,pidadr,prcnam,&retpcb,&ipid,&epid);
+  p=retpcb;
 
   if (!p) p=current;
 
-  p->pcb$b_prib=s->pri;
-  printk("here I am\n");
+  p->pcb$b_prib=31-pri;
+
+  if (p==current)
+    sch$change_cur_priority(p, p->pcb$b_prib);
+
+  sch$rse(p,PRI$_IOCOM,EVT$_SETPRI);
+
+  return SS$_NORMAL;
 }
