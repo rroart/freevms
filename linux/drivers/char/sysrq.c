7a9777b330d7575927647da8fd87abd7cedee692
Roar Thronæs <roart@nvg.ntnu.no>
2003-06-03 12:33:42 +0000

    not if CONFIG_VMS

--- linux/drivers/char/sysrq.c
+++ linux/drivers/char/sysrq.c
@@ -223,7 +223,9 @@ void do_emergency_sync(void) {
 static void sysrq_handle_sync(int key, struct pt_regs *pt_regs,
 		struct kbd_struct *kbd, struct tty_struct *tty) {
 	emergency_sync_scheduled = EMERG_SYNC;
+#ifndef CONFIG_VMS
 	wakeup_bdflush();
+#endif
 }
 static struct sysrq_key_op sysrq_sync_op = {
 	handler:	sysrq_handle_sync,
@@ -234,7 +236,9 @@ static struct sysrq_key_op sysrq_sync_op = {
 static void sysrq_handle_mountro(int key, struct pt_regs *pt_regs,
 		struct kbd_struct *kbd, struct tty_struct *tty) {
 	emergency_sync_scheduled = EMERG_REMOUNT;
+#ifndef CONFIG_VMS
 	wakeup_bdflush();
+#endif
 }
 static struct sysrq_key_op sysrq_mountro_op = {
 	handler:	sysrq_handle_mountro,
