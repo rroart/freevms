93f3bcd73746fa407866d5a00d867422b2adc8de
Roar Thronæs <roart@nvg.ntnu.no>
2003-06-03 12:41:32 +0000

    approx working sets.

--- sys/src/sysadjwsl.c
+++ sys/src/sysadjwsl.c
@@ -11,10 +11,15 @@
 #include<va_rangedef.h>
 #include<wsldef.h>
 
-int exe$adjwsl(int pagcnt, unsigned int *wsetlm) {
+int exe$adjwsl(signed int pagcnt, unsigned int *wsetlm) {
   struct _pcb * p=smp$gl_cpu_data[smp_processor_id()]->cpu$l_curpcb;
+  int newsize;
   //or ctl$gl_phd?
-  int newsize=p->pcb$l_phd->phd$l_wssize+pagcnt;
+  // do all here, have no priv image in setwork
+  p->pcb$l_phd->phd$l_wssize+=pagcnt;
+  p->pcb$l_phd->phd$l_wsquota+=pagcnt;
+  p->pcb$l_phd->phd$l_wsextent+=pagcnt;
+  newsize=p->pcb$l_phd->phd$l_wssize;
   if (pagcnt>0) {
     if (newsize>WSMAX) newsize=WSMAX;
     if (newsize>p->pcb$l_phd->phd$l_wsauthext) newsize=p->pcb$l_phd->phd$l_wsauthext;
