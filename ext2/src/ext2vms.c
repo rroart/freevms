c4d9c604b28d0bd6cf0047b6e0678ea6262661cb
Roar Thronæs <roart@nvg.ntnu.no>
2003-07-05 09:01:24 +0000

    attribs

--- ext2/src/ext2vms.c
+++ ext2/src/ext2vms.c
@@ -728,14 +728,14 @@ void exttwo_read_attrib(struct _fcb * fcb,struct inode * inode, struct _atrdef *
 	f->fat$b_rtype=FAT$C_SEQUENTIAL << 4;
 	f->fat$b_rattrib=0;
 	f->fat$w_rsize=0;
-	f->fat$l_hiblk=VMSSWAP(head->i_iblocks);
+	f->fat$l_hiblk=VMSSWAP(head->i_blocks);
 	f->fat$l_efblk=VMSSWAP(head->i_size/1024);
 	f->fat$w_ffbyte=head->i_size%1024;
 	f->fat$b_bktsize=0;
 	f->fat$b_vfcsize=0;
 	f->fat$w_maxrec=0;
 	f->fat$w_defext=0;
-	f->fat$w_gcb=0;
+	f->fat$w_gbc=0;
 	f->fat$w_versions=1;
 	//memcpy(atrp->atr$l_addr,&head->fh2$w_recattr,atrp->atr$w_size);
       }
