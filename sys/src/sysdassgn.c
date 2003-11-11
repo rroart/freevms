e88b7cd5062266d504baf5ec66c0fae6744104fe
Roar Thronæs <roart@nvg.ntnu.no>
2003-11-11 16:53:53 +0000

    add dassgn service

--- sys/src/sysdassgn.c
+++ sys/src/sysdassgn.c
@@ -14,6 +14,23 @@
 #include<linux/linkage.h>
 
 asmlinkage int exe$dassgn(unsigned short int chan) {
+  struct _ccb * ccb;
+  int sts = ioc$verify_chan(chan, &ccb);
+  if ((sts & 1) == 0) 
+    return sts;
+  // call exe$canceln, which is not yet implemented
+  // call verify_chan again, in case of asts
+  // check ccb$l_wind and close the file
+  // check ccb$w_ioc to see if outstanding io
+  // lock io db
+  ccb->ccb$b_amod=0;
+  // disassociate mailbox
+  // dec ucb ref count
+  ccb->ccb$l_ucb->ucb$l_refc--;
+  // checks and stuff to do if refc == 0
+  // do ioc$last_chan
+  // ioc$unlock
+  return SS$_NORMAL;
 }
 
 
