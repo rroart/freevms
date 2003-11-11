a8e830f386c102ca5bed85beef08afaa6305dd44
Roar Thronæs <roart@nvg.ntnu.no>
2003-11-11 16:53:06 +0000

    cluster io db changes. new better. routine for assign/dassgn

--- sys/src/iosubpagd.c
+++ sys/src/iosubpagd.c
@@ -12,6 +12,7 @@
 #include<ucbdef.h>
 #include<ddbdef.h>
 #include<ccbdef.h>
+#include<sbdef.h>
 #include<system_data_cells.h>
 #include<sysgen.h>
 #include<linux/vmalloc.h>
@@ -32,6 +33,8 @@ int ioc$searchdev() {
   /* implement later */
 }
 
+extern struct _sb othersb;
+
 // remember to make this interface like ioc_std$search in ioc_routines.h
 
 int ioc$search(struct return_values *r, void * devnam) {
@@ -41,11 +44,26 @@ int ioc$search(struct return_values *r, void * devnam) {
   /* return ucb or 0 */
   struct dsc$descriptor *s=devnam;
   struct _ddb * d=ioc$gl_devlist;
+  char * node = 0;
+  int nodelen = 0;
+  char * device = strchr(s->dsc$a_pointer,'$');
+  if (device) {
+    node=s->dsc$a_pointer;
+    nodelen=device-node;
+    device++;
+  } else {
+    device=s->dsc$a_pointer;
+  }
   do {
     //    printk("bcmp %s %s\n",d->ddb$t_name,s->dsc$a_pointer);
-    if (!bcmp(d->ddb$t_name,s->dsc$a_pointer,3/* was s->dsc$w_length*/)) {
+    struct _sb * sb=d->ddb$ps_sb;
+    if ((node==0) && sb)
+      goto next;
+    if (sb && 0!=strncmp(&sb->sb$t_nodename[1],node,nodelen))
+      goto next;
+    if (!bcmp(d->ddb$t_name,device,3/* was s->dsc$w_length*/)) {
       struct _ucb * tmp = d->ddb$l_ucb;
-      char * c=s->dsc$a_pointer;
+      char * c=device;
       char unit=c[3]-48;
       do {
 	//printk("unitcmp %x %x\n",unit,tmp->ucb$w_unit);
@@ -58,7 +76,18 @@ int ioc$search(struct return_values *r, void * devnam) {
       r->val1=tmp; // was d->ddb$l_ucb;
       return SS$_NORMAL;
     }
+  next:
     d=d->ddb$ps_link;
   } while (d && d!=ioc$gl_devlist);
   return SS$_NOSUCHDEV;
+}
+
+int ioc$verify_chan(unsigned short int chan, struct _ccb ** ccbp) {
+  struct _ccb * ccb=&ctl$gl_ccbbase[chan];
+  // also check channel num and access mode
+  if (ccb->ccb$b_amod==1) {
+    *ccbp=ccb;
+    return SS$_NORMAL;
+  } else 
+    return 0;
 }
