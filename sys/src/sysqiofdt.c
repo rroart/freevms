99ca15cbd308d9025b8c54d945d6f654f485e2b9
Roar Thronæs <roart@nvg.ntnu.no>
2004-01-16 13:49:22 +0000

    add incomplete checks

--- sys/src/sysqiofdt.c
+++ sys/src/sysqiofdt.c
@@ -3,3 +3,20 @@
 
 // Author. Roar Thronæs.
 
+#include <ssdef.h>
+#include <irpdef.h>
+
+int   exe_std$writechk (struct _irp *irp, struct _pcb *pcb, struct _ucb *ucb, void *buf, int bufsiz) {
+  // not fully implemented. lacks actual check?
+  if (bufsiz < 0) return SS$_BADPARAM;
+  irp->irp$l_bcnt=bufsiz;
+  return SS$_NORMAL;
+}
+
+int   exe_std$readchk (struct _irp *irp, struct _pcb *pcb, struct _ucb *ucb, void *buf, int bufsiz) {
+  // not fully implemented. lacks actual check?
+  if (bufsiz < 0) return SS$_BADPARAM;
+  irp->irp$l_bcnt=bufsiz;
+  return SS$_NORMAL;
+}
+
