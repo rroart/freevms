a16f56989cc5d5669e56cd3ce46901a99756dd01
Roar Thronæs <roart@nvg.ntnu.no>
2004-01-23 13:30:51 +0000

    *** empty log message ***

---
+++ starlet/src/msgdef.h
@@ -0,0 +1,112 @@
+#ifndef msgdef_h
+#define msgdef_h
+
+#define		MSG$_TRMUNSOLIC		1
+#define		MSG$_CRUNSOLIC		2
+#define		MSG$_DELPROC		3
+#define		MSG$_SNDSMB		4
+#define		MSG$_DEVOFFLIN		5
+#define		MSG$_TRMHANGUP		6
+#define		MSG$_DEVONLIN		7
+#define		MSG$_OPRQST		8
+#define		MSG$_OPREPLY		9
+
+#define		MSG$_SMBINI		8
+#define		MSG$_SMBDON		9
+#define		MSG$_SNDACC		10
+#define		MSG$_PURPROC		11
+#define		MSG$_DELIMAG		12
+#define		MSG$_PURIMAG		13
+#define		MSG$_SYSFUNC		14
+#define		MSG$_SNDJBC		15
+#define		MSG$_GETQUI		16
+
+#define		MSG$_XM_DATAVL		11
+#define		MSG$_XM_SHUTDN		12
+#define		MSG$_XM_ATTN		13
+
+#define		MSG$_INIOPR		16
+#define		MSG$_ABOOPR		17
+#define		MSG$_SUSOPR		18
+#define		MSG$_RESOPR		19
+#define		MSG$_DELSMB		20
+#define		MSG$_REQUE		21
+
+#define		MSG$_SMBRSP		32
+#define		MSG$_ACCRSP		33
+
+#define		MSG$_SCANBAD		40
+#define		MSG$_SCANRSP		41
+
+#define		MSG$_ABORT		48
+#define		MSG$_CONFIRM		49
+#define		MSG$_CONNECT		50
+#define		MSG$_DISCON		51
+#define		MSG$_EXIT		52
+#define		MSG$_INTMSG		53
+#define		MSG$_PATHLOST		54
+#define		MSG$_PROTOCOL		55
+#define		MSG$_REJECT		56
+#define		MSG$_THIRDPARTY		57
+#define		MSG$_TIMEOUT		58
+#define		MSG$_NETSHUT		59
+#define		MSG$_NODEACC		60
+#define		MSG$_NODEINACC		61
+#define		MSG$_EVTAVL		62
+#define		MSG$_EVTRCVCHG		63
+#define		MSG$_INCDAT		64
+#define		MSG$_RESET		65
+#define		MSG$_LINUP		66
+#define		MSG$_LINDWN		67
+#define		MSG$_EVTXMTCHG		68
+
+#define		MSG$_DEVOFFLINX		80
+#define		MSG$_WRONGVOL		81
+#define		MSG$_DEVWRTLCK		82
+#define		MSG$_TRMBRDCST		83
+#define		MSG$_MVCOMPLETE		84
+#define		MSG$_MVABORTED		85
+#define		MSG$_DISMOUNTED		86
+#define		MSG$_UDA50MVER		87
+#define		MSG$_DUPUNITNO		88
+#define		MSG$_CLUMBX		89
+#define		MSG$_TM78MVER		90
+#define		MSG$_SHAMEMFAL		91
+#define		MSG$_SHARDUCED		92
+#define		MSG$_RC25MVER		93
+#define		MSG$_RDRXMVER		94
+#define		MSG$_TU81MVER		95
+#define		MSG$_MAYAMVER		96
+#define		MSG$_SHACHASTA		97
+#define		MSG$_SHACOPCOM		98
+#define		MSG$_SHAREDZER		99
+#define		MSG$_SHAPOSMEM		100
+#define		MSG$_SHAALLCOM		101
+#define		MSG$_SHACOPBEG		102
+#define		MSG$_SHAWROMEM		103
+#define		MSG$_SHAORGMEM		104
+#define		MSG$_TRMLOSE_KB		105
+#define		MSG$_TRMGAIN_KB		106
+#define		MSG$_TRMSHRINK		107
+#define		MSG$_TRMEXPAND		108
+#define		MSG$_TRMRESIZE		109
+#define		MSG$_TRMMOVE		110
+#define		MSG$_SNDMME		111
+#define		MSG$_RETMME		112
+#define		MSG$_SHAADPBAD		113
+#define		MSG$_SHAEMUPTH		114
+#define		MSG$_SHABENODMP		115
+#define		MSG$_SHABNODMP		116
+#define		MSG$_SHALOCDEV		117
+#define		MSG$_SHANOBPOK		118
+#define		MSG$_SHABDMPOK		119
+#define		MSG$_BOOTDMPOK		120
+#define		MSG$_SHABDBCK		121
+#define		MSG$_SHANODMP		122
+#define		MSG$_SHADMPOK		123
+#define		MSG$_OPCFAO		124
+
+#define		MSG$_QMAN_INTERNAL	200
+		
+#endif
+		
