7601438e9591c28170b68621eeed9cb08e52049c
Roar Thronæs <roart@nvg.ntnu.no>
2004-01-23 09:18:08 +0000

    *** empty log message ***

---
+++ starlet/src/dscdef.h
@@ -0,0 +1,349 @@
+#ifndef dscdef_h
+#define dscdef_h
+
+#define		DSC$K_DTYPE_Z		0
+#define		DSC64$K_DTYPE_Z		0
+
+#define		DSC$K_DTYPE_BU		2
+#define		DSC64$K_DTYPE_BU	2
+#define		DSC$K_DTYPE_WU		3
+#define		DSC64$K_DTYPE_WU	3
+#define		DSC$K_DTYPE_LU		4
+#define		DSC64$K_DTYPE_LU	4
+#define		DSC$K_DTYPE_QU		5
+#define		DSC64$K_DTYPE_QU	5
+#define		DSC$K_DTYPE_OU		25
+#define		DSC64$K_DTYPE_OU	25
+#define		DSC$K_DTYPE_B		6
+#define		DSC64$K_DTYPE_B		6
+#define		DSC$K_DTYPE_W		7
+#define		DSC64$K_DTYPE_W		7
+#define		DSC$K_DTYPE_L		8
+#define		DSC64$K_DTYPE_L		8
+#define		DSC$K_DTYPE_Q		9
+#define		DSC64$K_DTYPE_Q		9
+#define		DSC$K_DTYPE_O		26
+#define		DSC64$K_DTYPE_O		26
+#define		DSC$K_DTYPE_F		10
+#define		DSC64$K_DTYPE_F		10
+#define		DSC$K_DTYPE_D		11
+#define		DSC64$K_DTYPE_D		11
+#define		DSC$K_DTYPE_G		27
+#define		DSC64$K_DTYPE_G		27
+#define		DSC$K_DTYPE_H		28
+#define		DSC64$K_DTYPE_H		28
+#define		DSC$K_DTYPE_FC		12
+#define		DSC64$K_DTYPE_FC	12
+#define		DSC$K_DTYPE_DC		13
+#define		DSC64$K_DTYPE_DC	13
+#define		DSC$K_DTYPE_GC		29
+#define		DSC64$K_DTYPE_GC	29
+#define		DSC$K_DTYPE_HC		30
+#define		DSC64$K_DTYPE_HC	30
+#define		DSC$K_DTYPE_FS		52
+#define		DSC64$K_DTYPE_FS	52
+#define		DSC$K_DTYPE_FT		53
+#define		DSC64$K_DTYPE_FT	53
+#define		DSC$K_DTYPE_FSC		54
+#define		DSC64$K_DTYPE_FSC	54
+#define		DSC$K_DTYPE_FTC		55
+#define		DSC64$K_DTYPE_FTC	55
+#define		DSC$K_DTYPE_FX		57
+#define		DSC64$K_DTYPE_FX	57
+#define		DSC$K_DTYPE_FXC		58
+#define		DSC64$K_DTYPE_FXC	58
+#define		DSC$K_DTYPE_CIT		31
+#define		DSC64$K_DTYPE_CIT	31
+#define		DSC$K_DTYPE_T		14
+#define		DSC64$K_DTYPE_T		14
+#define		DSC$K_DTYPE_VT		37
+#define		DSC64$K_DTYPE_VT	37
+#define		DSC$K_DTYPE_T2		38
+#define		DSC64$K_DTYPE_T2	38
+#define		DSC$K_DTYPE_NU		15
+#define		DSC64$K_DTYPE_NU	15
+#define		DSC$K_DTYPE_NL		16
+#define		DSC64$K_DTYPE_NL	16
+#define		DSC$K_DTYPE_NLO		17
+#define		DSC64$K_DTYPE_NLO	17
+#define		DSC$K_DTYPE_NR		18
+#define		DSC64$K_DTYPE_NR	18
+#define		DSC$K_DTYPE_NRO		19
+#define		DSC64$K_DTYPE_NRO	19
+#define		DSC$K_DTYPE_NZ		20
+#define		DSC64$K_DTYPE_NZ	20
+#define		DSC$K_DTYPE_P		21
+#define		DSC64$K_DTYPE_P		21
+#define		DSC$K_DTYPE_V		1
+#define		DSC64$K_DTYPE_V		1
+#define		DSC$K_DTYPE_VU		34
+#define		DSC64$K_DTYPE_VU	34
+#define		DSC$K_DTYPE_ZI		22
+#define		DSC64$K_DTYPE_ZI	22
+#define		DSC$K_DTYPE_ZEM		23
+#define		DSC64$K_DTYPE_ZEM	23
+#define		DSC$K_DTYPE_DSC		24
+#define		DSC64$K_DTYPE_DSC	24
+#define		DSC$K_DTYPE_BPV		32
+#define		DSC64$K_DTYPE_BPV	32
+#define		DSC$K_DTYPE_BLV		33
+#define		DSC64$K_DTYPE_BLV	33
+#define		DSC$K_DTYPE_ADT		35
+#define		DSC64$K_DTYPE_ADT	35
+#define		DSC$K_DTYPE_CAD		178
+#define		DSC64$K_DTYPE_CAD	178
+#define		DSC$K_DTYPE_ENT		179
+#define		DSC64$K_DTYPE_ENT	179
+#define		DSC$K_DTYPE_GBL		180
+#define		DSC64$K_DTYPE_GBL	180
+#define		DSC$K_DTYPE_EPT		181
+#define		DSC64$K_DTYPE_EPT	181
+#define		DSC$K_DTYPE_R11		182
+#define		DSC64$K_DTYPE_R11	182
+#define		DSC$K_DTYPE_FLD		183
+#define		DSC64$K_DTYPE_FLD	183
+#define		DSC$K_DTYPE_PCT		184
+#define		DSC64$K_DTYPE_PCT	184
+#define		DSC$K_DTYPE_DPC		185
+#define		DSC64$K_DTYPE_DPC	185
+#define		DSC$K_DTYPE_LBL		186
+#define		DSC64$K_DTYPE_LBL	186
+#define		DSC$K_DTYPE_SLB		187
+#define		DSC64$K_DTYPE_SLB	187
+#define		DSC$K_DTYPE_MOD		188
+#define		DSC64$K_DTYPE_MOD	188
+#define		DSC$K_DTYPE_EOM		189
+#define		DSC64$K_DTYPE_EOM	189
+#define		DSC$K_DTYPE_RTN		190
+#define		DSC64$K_DTYPE_RTN	190
+#define		DSC$K_DTYPE_EOR		191
+#define		DSC64$K_DTYPE_EOR	191
+
+#define		DSC$K_CLASS_Z		0
+#define		DSC64$K_CLASS_Z		0
+#define		DSC$K_CLASS_S		1
+#define		DSC64$K_CLASS_S		1
+#define		DSC$K_CLASS_D		2
+#define		DSC64$K_CLASS_D		2
+#define		DSC$K_CLASS_V		3
+#define		DSC64$K_CLASS_V		3
+#define		DSC$K_CLASS_A		4
+#define		DSC64$K_CLASS_A		4
+#define		DSC$K_CLASS_P		5
+#define		DSC64$K_CLASS_P		5
+#define		DSC$K_CLASS_PI		6
+#define		DSC64$K_CLASS_PI	6
+#define		DSC$K_CLASS_J		7
+#define		DSC64$K_CLASS_J		7
+#define		DSC$K_CLASS_JI		8
+#define		DSC$K_CLASS_SD		9
+#define		DSC64$K_CLASS_SD	9
+#define		DSC$K_CLASS_NCA		10
+#define		DSC64$K_CLASS_NCA	10
+#define		DSC$K_CLASS_VS		11
+#define		DSC64$K_CLASS_VS	11
+#define		DSC$K_CLASS_VSA		12
+#define		DSC64$K_CLASS_VSA	12
+#define		DSC$K_CLASS_UBS		13
+#define		DSC64$K_CLASS_UBS	13
+#define		DSC$K_CLASS_UBA		14
+#define		DSC64$K_CLASS_UBA	14
+#define		DSC$K_CLASS_SB		15
+#define		DSC64$K_CLASS_SB	15
+#define		DSC$K_CLASS_UBSB	16
+#define		DSC64$K_CLASS_UBSB	16
+
+#define		DSC$K_CLASS_BFA		191
+
+#define		DSC$K_Z_BLN		8
+#define		DSC$C_Z_BLN		8
+#define		DSC$K_S_BLN		8
+#define		DSC$C_S_BLN		8
+#define		DSC$K_D_BLN		8
+#define		DSC$C_D_BLN		8
+#define		DSC$K_P_BLN		8
+#define		DSC$C_P_BLN		8
+#define		DSC$K_J_BLN		8
+#define		DSC$C_J_BLN		8
+#define		DSC$K_VS_BLN		8
+#define		DSC$C_VS_BLN		8
+	
+#define		DSC$K_UBS_BLN		12
+#define		DSC$C_UBS_BLN		12
+	
+#define		DSC$K_SD_BLN		12
+#define		DSC$C_SD_BLN		12
+	
+#define		DSC$K_PI_BLN		12
+#define		DSC$C_PI_BLN		12
+#define		DSC$K_JI_BLN		12
+#define		DSC$C_JI_BLN		12
+	
+#define		DSC64$K_Z_BLN		24
+#define		DSC64$C_Z_BLN		24
+#define		DSC64$K_S_BLN		24
+#define		DSC64$C_S_BLN		24
+#define		DSC64$K_D_BLN		24
+#define		DSC64$C_D_BLN		24
+#define		DSC64$K_P_BLN		24
+#define		DSC64$C_P_BLN		24
+#define		DSC64$K_J_BLN		24
+#define		DSC64$C_J_BLN		24
+#define		DSC64$K_VS_BLN		24
+#define		DSC64$C_VS_BLN		24
+	
+#define		DSC64$K_UBS_BLN		32
+#define		DSC64$C_UBS_BLN		32
+	
+#define		DSC64$K_SD_BLN		32
+#define		DSC64$C_SD_BLN		32
+	
+#define		DSC64$K_PI_BLN		32
+#define		DSC64$C_PI_BLN		32
+#define		DSC64$K_JI_BLN		32
+#define		DSC64$C_JI_BLN		32
+
+struct _dscdef {
+  int dsc$a_handle;
+  unsigned int dsc$l_byteoff;
+  unsigned int dsc$l_logunit;
+  unsigned short int dsc$w_length;
+};
+
+struct _dscdef1 {
+  unsigned short int dsc$w_maxstrlen;
+  unsigned char dsc$b_dtype;
+  unsigned char dsc$b_class;
+  void *dsc$a_pointer;
+};
+
+struct _dscdef2 {
+  char dscdef$$_fill_3 [4];
+  void *dsc$a_base;
+};
+
+struct _dscdef3 {
+  char dscdef$$_fill_4 [8];
+  int dsc$l_pos;
+};
+
+struct _dscdef4 {
+  char dscdef$$_fill_5 [8];
+  unsigned short int dsc$w_maxlen;
+
+
+  short int dscdef$$_fill_1;
+};
+
+struct _dscdef5 {
+  char dscdef_64$$_fill_6 [8];
+  char dsc$b_scale;
+  unsigned char dsc$b_digits;
+  union  {
+    unsigned char dsc$b_aflags;
+    struct  {
+      unsigned dscdef$$_fill_2		: 4;
+      unsigned dsc$v_fl_redim		: 1;
+      unsigned dsc$v_fl_column		: 1;
+      unsigned dsc$v_fl_coeff		: 1;
+      unsigned dsc$v_fl_bounds		: 1;
+    };
+  };
+  unsigned char dsc$b_dimct;
+  unsigned int dsc$l_arsize;
+  void *dsc$a_a0;
+};
+ 
+struct _dscdef6 {
+  char dscdef$$_fill_7 [16];
+  int dsc$l_v0;
+  unsigned int dsc$l_s1;
+  unsigned int dsc$l_s2;
+};
+
+struct _dscdef7 {
+  char dscdef$$_fill_8 [20];
+  unsigned int dsc$l_m1;
+  unsigned int dsc$l_m2;
+};
+
+struct _dscdef8 {
+  char dscdef$$_fill_9 [8];
+  void *dsc$a_frame;
+};
+struct _dscdef9 {
+  char dscdef$$_fill_10 [8];
+  int dsc$l_sb_l1;
+  int dsc$l_sb_u1;
+};
+struct _dscdef10 {
+  char dscdef$$_fill_11 [12];
+  int dsc$l_ubsb_l1;
+  int dsc$l_ubsb_u1;
+};
+
+struct _dscdef64 {
+  char dscdef_64$$_fill_1 [8];
+  unsigned long long dsc64$q_length;
+};
+
+struct _dscdef1_64 {
+  unsigned short int dsc64$w_mbo;
+  unsigned char dsc64$b_dtype;
+  unsigned char dsc64$b_class;
+  int dsc64$l_mbmo;
+  unsigned long long dsc64$q_maxstrlen;
+  void *dsc64$pq_pointer;
+};
+
+struct _dscdef2_64 {
+  char dscdef_64$$_fill_3 [16];
+  void *dsc64$pq_base;
+};
+
+struct _dscdef3_64 {
+  char dscdef_64$$_fill_4 [24];
+  long long dsc64$q_pos;
+};
+
+struct _dscdef5_64 {
+  char dscdef_64$$_fill_6 [24];
+  char dsc64$b_scale;
+  unsigned char dsc64$b_digits;
+  union  {
+    unsigned char dsc64$b_aflags;
+    struct  {
+      unsigned dscdef_64$$_fill_2	: 4;
+      unsigned dsc64$v_fl_redim		: 1;
+      unsigned dsc64$v_fl_column	: 1;
+      unsigned dsc64$v_fl_coeff		: 1;
+      unsigned dsc64$v_fl_bounds	: 1;
+    };
+  };
+  unsigned char dsc64$b_dimct;
+  unsigned int dsc64$l_mbz;
+  unsigned long long dsc64$q_arsize;
+  void *dsc64$pq_a0;
+};
+ 
+struct _dscdef6_64 {
+  char dscdef_64$$_fill_7 [40];
+  long long dsc64$q_v0;
+  unsigned long long dsc64$q_s1;
+  unsigned long long dsc64$q_s2;
+};
+
+struct _dscdef7_64 {
+  char dscdef_64$$_fill_8 [48];
+  unsigned long long dsc64$q_m1;
+  unsigned long long dsc64$q_m2;
+};
+
+struct _dscdef10_64 {
+  char dscdef_64$$_fill_11 [32];
+  long long dsc64$q_ubsb_l1;
+  long long dsc64$q_ubsb_u1;
+};
+	
+#endif
+ 
