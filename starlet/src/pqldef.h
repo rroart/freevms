d7461505db2c71d740f89ab95b2e7a93888d2232
Roar Thronæs <roart@nvg.ntnu.no>
2004-01-26 16:15:36 +0000

    *** empty log message ***

---
+++ starlet/src/pqldef.h
@@ -0,0 +1,22 @@
+#ifndef pqldef_h
+#define pqldef_h
+
+#define	PQL$_LISTEND	0
+#define	PQL$_ASTLM	1
+#define	PQL$_BIOLM	2
+#define	PQL$_BYTLM	3
+#define	PQL$_CPULM	4
+#define	PQL$_DIOLM	5
+#define	PQL$_FILLM	6
+#define	PQL$_PGFLQUOTA	7
+#define	PQL$_PRCLM	8
+#define	PQL$_TQELM	9
+#define	PQL$_WSQUOTA	10
+#define	PQL$_WSDEFAULT	11
+#define	PQL$_ENQLM	12
+#define	PQL$_WSEXTENT	13
+#define	PQL$_JTQUOTA	14
+#define	PQL$_LENGTH	15
+	
+#endif
+	
