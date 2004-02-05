0e7c555586d61e38f28ea219ff6b99ee299eff87
Roar Thronæs <roart@nvg.ntnu.no>
2004-02-05 15:36:17 +0000

    oops wrong order

--- sys/src/exsubrout.c
+++ sys/src/exsubrout.c
@@ -15,10 +15,10 @@ void exe$instimq(struct _tqe * t) {
   if (times>=0 && times<5)     printk("%x %x %x\n",t,t->tqe$q_delta,t->tqe$q_time);
   if (times>=0 && times<5)     printk("%x %x %x\n",exe$gl_tqfl,exe$gl_tqfl->tqe$q_delta,exe$gl_tqfl->tqe$q_time);
   //  printk("+");
-  while (tmp!=exe$gl_tqfl && t->tqe$q_time<tmp->tqe$q_time)
+  while (tmp!=exe$gl_tqfl && t->tqe$q_time>tmp->tqe$q_time)
     tmp=tmp->tqe$l_tqfl;
   //  printk("-");
-  insque(t,tmp);
+  insque(t,tmp->tqe$l_tqbl);
   //  printk("0");
   if (times>=0 && times<5)  printk("%x %x %x\n",exe$gl_tqfl,exe$gl_tqfl->tqe$q_delta,exe$gl_tqfl->tqe$q_time);
   vmsunlock(&SPIN_TIMER,savipl);
