9ed74b9dd3c9db2b20828a0f8fb152e14cc2dd5a
Roar Thronæs <roart@nvg.ntnu.no>
2004-02-17 16:17:54 +0000

    working rmvtimq

--- sys/src/exsubrout.c
+++ sys/src/exsubrout.c
@@ -11,28 +11,39 @@ void exe$instimq(struct _tqe * t) {
   static signed int times=-500;
   struct _tqe *tmp=exe$gl_tqfl->tqe$l_tqfl;
   int savipl=vmslock(&SPIN_TIMER,IPL$_TIMER);
+#if 0
   times++;
   if (times>=0 && times<5)     printk("%x %x %x\n",t,t->tqe$q_delta,t->tqe$q_time);
   if (times>=0 && times<5)     printk("%x %x %x\n",exe$gl_tqfl,exe$gl_tqfl->tqe$q_delta,exe$gl_tqfl->tqe$q_time);
   //  printk("+");
+#endif
   while (tmp!=exe$gl_tqfl && t->tqe$q_time>tmp->tqe$q_time)
     tmp=tmp->tqe$l_tqfl;
   //  printk("-");
   insque(t,tmp->tqe$l_tqbl);
   //  printk("0");
+#if 0
   if (times>=0 && times<5)  printk("%x %x %x\n",exe$gl_tqfl,exe$gl_tqfl->tqe$q_delta,exe$gl_tqfl->tqe$q_time);
+#endif
   vmsunlock(&SPIN_TIMER,savipl);
 }
 
-void exe$rmvtimq(unsigned long reqidt, unsigned int acmode) {
-  struct _tqe *tmp=exe$gl_tqfl->tqe$l_tqfl;
+void exe_std$rmvtimq(int acmode, int reqid, int remval, int ipid) {
+  struct _tqe *tmp=exe$gl_tqfl->tqe$l_tqfl,*next;
   //int savipl=vmslock(&SPIN_TIMER,IPL$_TIMER);
   while (tmp!=exe$gl_tqfl) {
-    if (reqidt==tmp->tqe$l_astprm) {
+    next=tmp->tqe$l_tqfl;
+    if (reqid && reqid==tmp->tqe$l_astprm) {
       remque(tmp,0);
-      vfree(tmp);
+      kfree(tmp);
     }
-    tmp=tmp->tqe$l_tqfl;
+    if (ipid && ipid==tmp->tqe$l_pid) {
+      if (tmp->tqe$b_rqtype=remval) {
+	remque(tmp,0);
+	kfree(tmp);
+      }
+    }
+    tmp=next;
   cont:
   }
 }
