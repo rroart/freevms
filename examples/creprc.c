c8c2ae053fee99c7ab2fb764219e774d872036f9
Roar Thronæs <roart@nvg.ntnu.no>
2004-07-23 13:33:12 +0000

    creprc added

--- examples/creprc.c
+++ examples/creprc.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
 #include <ssdef.h>
 #include <descrip.h>
+#include <syidef.h>
+#include <misc.h>
 
 main(){
   $DESCRIPTOR(imgname,"/bin/ls");
   $DESCRIPTOR(prcname,"ls");
+  $DESCRIPTOR(imgname2,"/vms$common/syshlp/examples/hi.exe");
+  $DESCRIPTOR(imgname3,"[vms$common.syshlp.examples]hi.exe");
+  $DESCRIPTOR(prcname3,"hi");
   int pid;
+  struct item_list_3 lst[14], syilst[2];
 
   int RC = sys$creprc(&pid, &imgname,0,0,0,
 		      0, 0, &prcname, 24, 0,
 		      0, 0, 0, 0, 0);
 
   printf("creprc pid %x returned %x\n",pid,RC);
+
+  int retlenaddr;
+  int mem=0;
+
+  syilst[0].buflen=4;
+  syilst[0].item_code=SYI$_LASTFLD;
+  syilst[0].bufaddr=&mem;
+  syilst[0].retlenaddr=&retlenaddr;
+  syilst[1].buflen=0;
+  syilst[1].item_code=0;
+
+  int sts=sys$getsyi(0,0,0,syilst,0,0,0);
+
+  if (!mem) {
+    printf("No VMS MM\n");
+    goto out;
+  }
+
+  int file = fopen(imgname2.dsc$a_pointer,"r");
+  if (file==0) {
+    printf("%s not found\n",imgname2.dsc$a_pointer);
+    goto out;
+  }
+
+  RC = sys$creprc(&pid, &imgname3,0,0,0,
+		      0, 0, &prcname3, 24, 0,
+		      0, 0, 0, 0, 0);
+
+  printf("creprc pid %x returned %x\n",pid,RC);
+
+ out:
+  sleep(60);
 
   return SS$_NORMAL;
 }
