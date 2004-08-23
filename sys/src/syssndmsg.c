1476ab59b18e58ee21682663fb5729745b029bb3
Roar Thronæs <roart@nvg.ntnu.no>
2004-08-23 08:39:57 +0000

    needed another structure inside

--- sys/src/syssndmsg.c
+++ sys/src/syssndmsg.c
@@ -2,10 +2,13 @@
 #include<linux/kernel.h>
 
 #include<descrip.h>
+#include<opcdef.h>
 
 asmlinkage int exe$sndopr(void *msgbuf, unsigned short int chan) {
      struct dsc$descriptor * d=msgbuf;
-     printk("%s\n",d->dsc$a_pointer);
+     struct _opcdef * msg = d->dsc$a_pointer;
+
+     printk("%s\n",&msg->opc$l_ms_text);
      // reimplement this
 }
 
