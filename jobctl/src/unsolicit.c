07774a112bb904419298c788a51e5dcf6205367d
Roar Thronæs <roart@nvg.ntnu.no>
2004-08-23 08:29:31 +0000

    add jobctl

---
+++ jobctl/src/unsolicit.c
@@ -0,0 +1,28 @@
+#include <descrip.h>
+#include <prcdef.h>
+#include <prvdef.h>
+#include <sysgen.h>
+
+// This shall later be implemented in a process
+
+int jobctl_unsolicit(struct dsc$descriptor * term) {
+  printk("<0>" "soon to creprc, term = %s\n",term->dsc$a_pointer);
+  // in addition to term some more data is ordinarily sent to this routine.
+  // normally received by a permanent mailbox
+  // not yet $DESCRIPTOR(log, "[vms$common.sysexe]loginout");
+   $DESCRIPTOR(log, "[vms$common.sysexe]dcl");
+  int defpri = DEFPRI;
+  int uic=0x00010004;
+  long long priv = PRV$M_TMPMBX | PRV$M_NETMBX | PRV$M_SETPRV;
+  int sts = PRC$M_INTER;
+  struct dsc$descriptor prcnam;
+  char nam[16]="_";
+
+  prcnam.dsc$w_length=1+term->dsc$w_length;
+  prcnam.dsc$a_pointer=nam;
+
+  memcpy(&nam[1],term->dsc$a_pointer, term->dsc$w_length);
+
+  sts = exe$creprc(0, log, term, term, term, &priv, 0, &prcnam, defpri, uic, 0, sts);
+  return sts;
+}
