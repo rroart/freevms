69eee21d3e743fe7de3a313d3191a502cfe0dc72
Roar Thronæs <roart@nvg.ntnu.no>
2004-09-01 13:33:25 +0000

    new stuff. duplicate typeahead creation for now.

--- ttdrvr/src/ttyfdt.c
+++ ttdrvr/src/ttyfdt.c
@@ -20,18 +20,55 @@
 #include<linux/pci.h>
 #include<system_service_setup.h>
 #include<descrip.h>
+#include<ttyucbdef.h>
 
 #include<linux/blkdev.h>
 
+// maybe use _twp and _tt_readbuf for these two?
+int xyz=0;
 int tty$fdtread(struct _irp * i, struct _pcb * p, struct _ucb * u, struct _ccb * c){
+  int sts;
 
+  int *buf,size;
+  size=i->irp$l_qio_p2;
+  buf = i->irp$l_qio_p1;
+  exe_std$readchk(i,p,u,buf,size);
+
+  struct _ltrm_ucb * lt = u;
+
+  // check whether something we set earlier is consistent
+  if (lt->ucb$l_tl_phyucb) {
+    struct _tty_ucb * tty = lt->ucb$l_tl_phyucb;
+    if (u!=tty->ucb$l_tt_logucb)
+      printk("<0>" "tty error in read\n");
+  }
+
+  sts = exe$qiodrvpkt (i,p,u);
+  return sts;
 }
 
 int tty$fdtwrite(struct _irp * i, struct _pcb * p, struct _ucb * u, struct _ccb * c){
   int sts;
 
-  sts = exe$altquepkt (i,p,u);
+  int *buf,size;
+  size=i->irp$l_qio_p2;
+  buf = i->irp$l_qio_p1;
+  exe_std$writechk(i,p,u,buf,size);
 
+  struct _ltrm_ucb * lt = u;
+
+  // check whether something we set earlier is consistent
+  if (lt->ucb$l_tl_phyucb) {
+    struct _tty_ucb * tty = lt->ucb$l_tl_phyucb;
+    if (u!=tty->ucb$l_tt_logucb)
+      printk("<0>" "tty error in write\n");
+  }
+
+  // not yet
+  // no full duplex with untested altquepkt, using qiodrvpkt instead
+  //  sts = exe$altquepkt (i,p,u);
+
+  sts = exe$qiodrvpkt (i,p,u);
   return sts;
 }
 
--- ttdrvr/src/ttystrstp.c
+++ ttdrvr/src/ttystrstp.c
@@ -20,35 +20,99 @@
 #include<linux/pci.h>
 #include<system_service_setup.h>
 #include<descrip.h>
-
-#include<linux/blkdev.h>
+#include <ttyucbdef.h>
+#include <ttyvecdef.h>
+#include <ttytadef.h>
 
 unsigned long tty$startio (struct _irp * i, struct _ucb * u)
 {
   unsigned long sts=SS$_NORMAL;
-  struct request * rq = kmalloc (sizeof(struct request), GFP_KERNEL);
 
-  //printk("ide startio %x %x %x\n",i->irp$l_qio_p1,i->irp$l_qio_p2,i->irp$l_qio_p3);
+  struct _ltrm_ucb * lt = u;
 
-  //  ide_drive_t *drive=u->ucb$l_orb;
+  struct _ucb * phy;
 
-  //i->irp$l_qio_p3 <<= 1;
-  //  i->irp$l_qio_p3 += partadd(u->ucb$l_orb,i->irp$l_qio_p4);
-    //drive->part[i->irp$l_qio_p4].start_sect + drive->sect0;
+  phy = lt->ucb$l_tl_phyucb;
 
-  //  ideu=u;
+  if (phy==0) {
+    printk("<0>" "PHY 0 %x %s\n",lt,&u->ucb$l_ddb->ddb$t_name[1]);
+    return SS$_NORMAL;
+  }
+
+  u=phy;
+
+  u->ucb$l_svapte=i->irp$l_svapte;
+  u->ucb$l_boff=i->irp$l_boff;
+  u->ucb$l_bcnt=i->irp$l_bcnt;
 
   switch (i->irp$v_fcode) {
 
     case IO$_WRITELBLK:
     case IO$_WRITEVBLK:
-    case IO$_WRITEPBLK: {
-    }
+    case IO$_WRITEPBLK:
+      {
+	struct _tty_ucb * tty = u;
+	struct _tt_port * ttp = tty->ucb$l_tt_port;
+	int (*fn)();
+	fn = ttp->port_start_read;
+	fn(); // not yet?
 
+	if (tty->ucb$l_tt_typahd==0) {  // this is duplicated -> own routine?
+	  tty->ucb$l_tt_typahd = kmalloc (sizeof (struct _tt_type_ahd),GFP_KERNEL);
+	  struct _tt_type_ahd * ahd = tty->ucb$l_tt_typahd;
+	  ahd->tty$l_ta_data=kmalloc(1024,GFP_KERNEL);
+	  ahd->tty$l_ta_get=ahd->tty$l_ta_put=ahd->tty$l_ta_data;
+	  ahd->tty$l_ta_end=(long)ahd->tty$l_ta_data+1024;
+	  // the book says this should fork and dismiss character
+	  tty->ucb$b_tt_outype=0;
+	  // not  yet? : return;
+	}
+	
+	int chr;
+	tty$putnextchar(&chr,u);
+
+	tty=u;
+	if (tty->ucb$b_tt_outype==0) //check
+	  return sts;
+	fn = ttp->port_startio;
+	sts = fn(i,u);
+	return sts;
+      }
+      break;
+      
     case IO$_READLBLK:
     case IO$_READVBLK:
-    case IO$_READPBLK: {
-    }
+    case IO$_READPBLK:
+      {
+	struct _tty_ucb * tty = u;
+	struct _tt_port * ttp = tty->ucb$l_tt_port;
+	int (*fn)();
+	fn = ttp->port_start_read;
+	fn(); // not yet?
+
+	if (tty->ucb$l_tt_typahd==0) {  // this is duplicated -> own routine?
+	  tty->ucb$l_tt_typahd = kmalloc (sizeof (struct _tt_type_ahd),GFP_KERNEL);
+	  struct _tt_type_ahd * ahd = tty->ucb$l_tt_typahd;
+	  ahd->tty$l_ta_data=kmalloc(1024,GFP_KERNEL);
+	  ahd->tty$l_ta_get=ahd->tty$l_ta_put=ahd->tty$l_ta_data;
+	  ahd->tty$l_ta_end=(long)ahd->tty$l_ta_data+1024;
+	  // the book says this should fork and dismiss character
+	  tty->ucb$b_tt_outype=0;
+	  // not  yet? : return;
+	}
+	
+	int chr;
+	int CC;
+	tty$getnextchar(&chr,&CC,u);
+
+	tty=u;
+	if (tty->ucb$b_tt_outype==0) //check
+	  return sts;
+	fn = ttp->port_startio;
+	sts = fn(i,u);
+	return sts;
+      }
+      break;
 
     /* Who knows what */
 
