55d825fdb0c0b64c771cf292e68c6b3be351f15f
Roar Thronæs <roart@nvg.ntnu.no>
2004-09-19 11:49:59 +0000

    try to do duplex. do not start if interrupt expected

--- ttdrvr/src/ttystrstp.c
+++ ttdrvr/src/ttystrstp.c
@@ -79,7 +79,7 @@ unsigned long tty$startio (struct _irp * i, struct _ucb * u)
 	tty->tty$v_st_write = 1;
 	tty->ucb$l_tt_wrtbuf=i->irp$l_svapte;
 
-	// not yet? if (u->ucb$w_sts&UCB$M_INT) return; // here?
+	if (u->ucb$l_sts&UCB$M_INT) return; // here?
 
 	int chr;
 	int CC;
@@ -117,7 +117,7 @@ unsigned long tty$startio (struct _irp * i, struct _ucb * u)
 	  // not  yet? : return;
 	}
 	
-	// not yet? if (u->ucb$w_sts&UCB$M_INT) return; // here?
+	if (u->ucb$l_sts&UCB$M_INT) return; // here?
 
 	int chr;
 	int CC;
@@ -140,5 +140,108 @@ unsigned long tty$startio (struct _irp * i, struct _ucb * u)
   }
 }
 
+// got to move some parts some time
 int tty$wrtstartio(struct _irp * i,struct _ucb * u) {
+  unsigned long sts=SS$_NORMAL;
+
+  struct _ltrm_ucb * lt = u;
+
+  struct _ucb * phy;
+
+  phy = lt->ucb$l_tl_phyucb;
+
+  if (phy==0) {
+    printk("<0>" "PHY 0 %x %s\n",lt,&u->ucb$l_ddb->ddb$t_name[1]);
+    return SS$_NORMAL;
+  }
+
+  u=phy;
+
+  switch (i->irp$v_fcode) {
+
+    case IO$_WRITELBLK:
+    case IO$_WRITEVBLK:
+    case IO$_WRITEPBLK:
+      {
+	struct _tty_ucb * tty = u;
+	struct _tt_port * ttp = tty->ucb$l_tt_port;
+	int (*fn)();
+	fn = ttp->port_start_read;
+	fn(); // not yet?
+
+	if (tty->ucb$l_tt_typahd==0) {  // this is duplicated -> own routine?
+	  tty->ucb$l_tt_typahd = kmalloc (sizeof (struct _tt_type_ahd),GFP_KERNEL);
+	  struct _tt_type_ahd * ahd = tty->ucb$l_tt_typahd;
+	  memset(ahd, 0, sizeof(struct _tt_type_ahd));
+	  ahd->tty$b_ta_type = DYN$C_TYPAHD;
+	  ahd->tty$l_ta_data=kmalloc(1024,GFP_KERNEL);
+	  ahd->tty$l_ta_get=ahd->tty$l_ta_put=ahd->tty$l_ta_data;
+	  ahd->tty$l_ta_end=(long)ahd->tty$l_ta_data+1024;
+	  // the book says this should fork and dismiss character
+	  tty->ucb$b_tt_outype=0;
+	  // not  yet? : return;
+	}
+
+	tty->tty$v_st_write = 1;
+	tty->ucb$l_tt_wrtbuf=i->irp$l_svapte;
+
+	if (u->ucb$l_sts&UCB$M_INT) return; // here?
+
+	int chr;
+	int CC;
+	tty$getnextchar(&chr, &CC, u);
+
+	tty=u;
+	if (tty->ucb$b_tt_outype==0) //check
+	  return sts;
+	fn = ttp->port_startio;
+	sts = fn(chr,u,CC); // check 
+	return sts;
+      }
+      break;
+      
+    case IO$_READLBLK:
+    case IO$_READVBLK:
+    case IO$_READPBLK:
+      {
+	struct _tty_ucb * tty = u;
+	struct _tt_port * ttp = tty->ucb$l_tt_port;
+	int (*fn)();
+	fn = ttp->port_start_read;
+	fn(); // not yet?
+
+	if (tty->ucb$l_tt_typahd==0) {  // this is duplicated -> own routine?
+	  tty->ucb$l_tt_typahd = kmalloc (sizeof (struct _tt_type_ahd),GFP_KERNEL);
+	  struct _tt_type_ahd * ahd = tty->ucb$l_tt_typahd;
+	  memset(ahd, 0, sizeof(struct _tt_type_ahd));
+	  ahd->tty$b_ta_type = DYN$C_TYPAHD;
+	  ahd->tty$l_ta_data=kmalloc(1024,GFP_KERNEL);
+	  ahd->tty$l_ta_get=ahd->tty$l_ta_put=ahd->tty$l_ta_data;
+	  ahd->tty$l_ta_end=(long)ahd->tty$l_ta_data+1024;
+	  // the book says this should fork and dismiss character
+	  tty->ucb$b_tt_outype=0;
+	  // not  yet? : return;
+	}
+	
+	if (u->ucb$l_sts&UCB$M_INT) return; // here?
+
+	int chr;
+	int CC;
+	tty$getnextchar(&chr, &CC, u);
+
+	tty=u;
+	if (tty->ucb$b_tt_outype==0) //check
+	  return sts;
+	fn = ttp->port_startio;
+	sts = fn(i,u);
+	return sts;
+      }
+      break;
+
+    /* Who knows what */
+
+    default: {
+      return (SS$_BADPARAM);
+    }
+  }
 }
