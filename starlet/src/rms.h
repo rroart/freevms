dd6c730d92395ec400feb805864b97e42a7180eb
Roar Thronæs <roart@nvg.ntnu.no>
2004-11-04 13:05:14 +0000

    *** empty log message ***

---
+++ starlet/src/rms.h
@@ -0,0 +1,10 @@
+#ifndef rms_h
+#define rms_h
+
+# include <namdef.h>
+# include <fabdef.h>
+# include <rabdef.h>
+# include <xab.h>
+# include <rmsdef.h>
+
+#endif
---
+++ starlet/src/xab.h
@@ -0,0 +1,19 @@
+#ifndef xab_h
+#define xab_h
+
+#include <xaballdef.h>
+#include <xabcxfdef.h>
+#include <xabcxrdef.h>
+#include <xabdatdef.h>
+#include <xabdef.h>
+#include <xabfhcdef.h>
+#include <xabitmdef.h>
+#include <xabjnldef.h>
+#include <xabkeydef.h>
+#include <xabprodef.h>
+#include <xabrdtdef.h>
+#include <xabrudef.h>
+#include <xabsumdef.h>
+#include <xabtrmdef.h>
+
+#endif
