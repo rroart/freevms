08e19e880a3bfa30adcba1607efb6a9c161ce211
Roar Thronæs <roart@nvg.ntnu.no>
2004-11-21 11:59:33 +0000

    use another lib routine

--- cliutl/src/create.c
+++ cliutl/src/create.c
@@ -10,11 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc>2 && 0==strncmp(argv[1],"/directory",strlen(argv[1]))) {
+  setvbuf(stdout, NULL, _IONBF, 0);      // need this to see i/o at all
+  if (argc>1 && 0==strncmp(argv[1],"/directory",strlen(argv[1]))) {
     struct dsc$descriptor d;
     d.dsc$a_pointer=argv[2];
     d.dsc$w_length=strlen(argv[2]);
-    lib$create_dir(&d);
+    // not yet    lib$create_dir(&d);
+    lib$create_dir2(argv[2],1);
     return SS$_NORMAL;
   }
   fprintf(stderr, "%CREATE-E-Can not create file yet\n");
