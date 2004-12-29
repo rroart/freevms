30275b6d352ce86f6e1135f49bd206f09163e3ae
Roar Thronæs <roart@nvg.ntnu.no>
2004-12-29 11:53:39 +0000

    add null check. (from allison)

--- librtl/src/str_get1_dx.c
+++ librtl/src/str_get1_dx.c
@@ -1,6 +1,35 @@
 /*
- * str.c
+ *	strget1_dx
  *
+ *	Copyright (C) 2003 Andrew Allison
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *The authors may be contacted at:
+ *
+ *	Andrew Allison		freevms@sympatico.ca
+ *
+ *				Andrew Allison
+ *				50 Denlaw Road
+ *				London, Ont
+ *				Canada 
+ *				N6G 3L4
+ *
+ */
+
+ /*
  *	Code for VAX STR$GET1_DX routine
  *
  * Description:
@@ -16,6 +45,12 @@
  *
  *	Feb 4, 1997 - Kevin Handy
  *		Include "stdlib.h" to lose warnings with '-Wall'.
+ *
+ *	Feb 26, 2004 - Andrew Allison
+ *		Added GNU License
+ *
+ *	Mar 1, 2004 - Andrew Allison
+ *		Fixed glitch, if asked for 0 bytes calloc return a value
  */
 
 #include <stdio.h>
@@ -41,60 +76,75 @@
 unsigned long str$get1_dx(unsigned short* word_integer_length,
 	struct dsc$descriptor_s* character_string)
 {
-	char* new_memory;
+char* new_memory;
 
+new_memory = NULL;
+/*
+ * Validate string class
+ */
+if (character_string->dsc$b_class != DSC$K_CLASS_D)
+{
+	DOSIGNAL(STR$_ILLSTRCLA);
+	return(STR$_ILLSTRCLA);
+}
+/*
+ * If memory is already allocated, redo the allocation
+ */
+if (character_string->dsc$a_pointer != NULL)
+{
 	/*
-	 * Validate string class
+	 * Do we really need to change anything?
 	 */
-	if (character_string->dsc$b_class != DSC$K_CLASS_D)
+	if (character_string->dsc$w_length != *word_integer_length)
 	{
-		DOSIGNAL(STR$_ILLSTRCLA);
-		return(STR$_ILLSTRCLA);
-	}
-
-	/*
-	 * If memory is already allocated, redo the allocation
-	 */
-	if (character_string->dsc$a_pointer != NULL)
-	{
+	int	i;
 		/*
-		 * Do we really need to change anything?
+		 * Reallocate old memory
 		 */
-		if (character_string->dsc$w_length != *word_integer_length)
+		if ( *word_integer_length == 0 )
 		{
-			/*
-			 * Reallocate old memory
-			 */
-			new_memory = (char*)realloc(character_string->dsc$a_pointer,
-				*word_integer_length);
-			if (new_memory == NULL)
-			{
-				DOSIGNAL(STR$_INSVIRMEM);
-				return STR$_INSVIRMEM;
-			}
-			character_string->dsc$a_pointer = new_memory;
-			character_string->dsc$w_length = *word_integer_length;
+			free(character_string->dsc$a_pointer);
+			character_string->dsc$a_pointer = NULL;
+			character_string->dsc$w_length  = *word_integer_length;
 		}
-	}
-	else
-	{
-		/*
-		 * Allocate some new memory
-		 */
-		character_string->dsc$a_pointer =
-			(char*)malloc(*word_integer_length);
-		if (character_string->dsc$a_pointer == NULL)
+		else
+		{
+		new_memory = (char*)calloc(*word_integer_length,1);
+
+		if ((new_memory == NULL) && (*word_integer_length != 0))
 		{
 			DOSIGNAL(STR$_INSVIRMEM);
 			return STR$_INSVIRMEM;
 		}
-		character_string->dsc$w_length = *word_integer_length;
-	}
+		for (i=0; i < *word_integer_length; i++)
+		    new_memory[i] = character_string->dsc$a_pointer[i];
 
+		free(character_string->dsc$a_pointer);
+		character_string->dsc$a_pointer = new_memory;
+		character_string->dsc$w_length  = *word_integer_length;
+		}
+	}
+}
+else
+{
 	/*
-	 * Successful
+	 * Allocate some new memory
 	 */
-	return STR$_NORMAL;
+
+	if (*word_integer_length != 0 )
+		character_string->dsc$a_pointer = (char*)calloc(*word_integer_length,1);
+	if ((character_string->dsc$a_pointer == NULL) && (*word_integer_length != 0 ))
+	{
+		DOSIGNAL(STR$_INSVIRMEM);
+		return STR$_INSVIRMEM;
+	}
+	character_string->dsc$w_length = *word_integer_length;
+}
+
+/*
+ * Successful
+ */
+return STR$_NORMAL;
 }
 
 
