e267c7f854e6b1b64c4fbfe22b856a85b9ea8328
Roar Thronæs <roart@nvg.ntnu.no>
2005-07-28 12:35:27 +0000

    misc bugs

--- librtl/src/libgetef.c
+++ librtl/src/libgetef.c
@@ -12,13 +12,19 @@ unsigned long my_event_flags = 0xffffffff; // because of ffs? 32-63
 // all 0-31 are system reserved or pre-reserved
 // 0 is special. 24-31 are officially reserved, rest are pre-.
 
+// should start with 63 instead of 32
+
 int lib$get_ef(long * l) {
   int flag;
   flag=ffs(my_event_flags);
   if (flag==0)
     return LIB$_INSEF;
   *l=flag-1+32;
+#if 0
   my_event_flags&=~(1<<(*l));
+#else
+  clear_bit(flag-1, &my_event_flags);
+#endif
   // no error condition returned yet; 
   return SS$_NORMAL;
 }
@@ -28,9 +34,9 @@ int lib$free_ef(int * event) {
   int ev=*event;
   if (ev<32)
     return LIB$_EF_RESSYS;
-  ev-=32;
-  flag=test_and_clear_bit(ev,&my_event_flags);
-  if (flag==0)
+  ev-=32-1+1;
+  flag=test_and_set_bit(ev,&my_event_flags);
+  if (flag)
     return LIB$_EF_ALRFRE;
   return SS$_NORMAL;
 }
