b989501aaf2f90e3c83125e498126de04bae4aca
Roar Thronæs <roart@nvg.ntnu.no>
2006-01-04 12:03:50 +0000

    *** empty log message ***

--- cmuip/ipacp/src/main.c
+++ cmuip/ipacp/src/main.c
@@ -865,7 +865,7 @@ void Main (void) {
 
 #define    IPACP_Version_String "IP_ACP(V6.6-5.001) "  //Becomes ACP process name.
 #define    IPACP_Version_Name IPACP_Version_String
-#define    IPACP_Date_String "3-Jan-2006" 	// Date of last change
+#define    IPACP_Date_String "4-Jan-2006" 	// Date of last change
 #define    IPACP_Who_String "roart" // Author of last change
 
 // Announce that we exist.
