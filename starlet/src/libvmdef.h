8d23da8c20aded127fc68943ff4d63fe9641bb5d
Roar Thronæs <roart@nvg.ntnu.no>
2006-09-03 13:29:45 +0000

    *** empty log message ***

---
+++ starlet/src/libvmdef.h
@@ -0,0 +1,19 @@
+#ifndef libvmdef_h
+#define libvmdef_h
+
+#define		LIB$K_VM_FIRST_FIT		1
+#define		LIB$K_VM_QUICK_FIT		2
+#define		LIB$K_VM_FREQ_SIZES		3
+#define		LIB$K_VM_FIXED			4
+
+#define		LIB$M_VM_BOUNDARY_TAGS		1
+#define		LIB$M_VM_GET_FILL0		2
+#define		LIB$M_VM_GET_FILL1		4
+#define		LIB$M_VM_FREE_FILL0		8
+#define		LIB$M_VM_FREE_FILL1		16
+#define		LIB$M_VM_EXTEND_AREA		32
+#define		LIB$M_VM_NO_EXTEND		64
+#define		LIB$M_VM_TAIL_LARGE		128
+
+#endif
+ 
