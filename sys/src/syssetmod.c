1a7e3eb8bd2e19e5b0cf4492f169bd46bacc1fae
Roar Thronæs <roart@nvg.ntnu.no>
2008-01-06 07:59:05 +0000

    implemented setswm service

--- sys/src/syssetmod.c
+++ sys/src/syssetmod.c
@@ -5,8 +5,13 @@
 
 #include<linux/linkage.h>
 #include<linux/kernel.h>
-
-#include <ssdef.h>
+#include<linux/sched.h>
+#include<internals.h>
+#include<ipldef.h>
+#include<pcbdef.h>
+#include<prvdef.h>
+#include<ssdef.h>
+#include<system_data_cells.h>
 
 asmlinkage int exe$setrwm(int flags) {
   printk("setrwm not implemented\n");
@@ -14,6 +19,15 @@ asmlinkage int exe$setrwm(int flags) {
 }
 
 asmlinkage int exe$setswm(int flags) {
-  printk("setswm not implemented\n");
+  int priv = ctl$gl_pcb->pcb$l_priv;
+  if ((priv & PRV$M_PSWAPM) == 0)
+    return SS$_NOPRIV; // check
+  int oldipl = vmslock(&SPIN_SCHED, IPL$_SCHED);
+  if (flags)
+    ctl$gl_pcb->pcb$l_sts |= PCB$M_PSWAPM;
+  else
+    ctl$gl_pcb->pcb$l_sts &= ~PCB$M_PSWAPM;
+  vmsunlock(&SPIN_SCHED, oldipl);
+  return SS$_NORMAL;
 }
 
