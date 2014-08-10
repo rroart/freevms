162090c61d93bef12f85537064c85a6f801e742a
Roar Thronæs <rroart@gmail.com>
2014-08-10 15:11:25 +0200

    gcc more picky about anonymous struct unions, as maybe it should

--- sys/src/vmspte.h
+++ sys/src/vmspte.h
@@ -36,40 +36,40 @@ struct _mypte {
       unsigned pte$v_fill : 1;
     };
     struct {
-      unsigned page_bits : 9; //refer to pgtable.h for these
-      unsigned yet_unused : 1;
-      unsigned pte$v_typ0 : 1;
-      unsigned pte$v_typ1 : 1;
+      unsigned page_bits1 : 9; //refer to pgtable.h for these
+      unsigned yet_unused1 : 1;
+      unsigned pte$v_typ01 : 1;
+      unsigned pte$v_typ11 : 1;
       unsigned pte$v_pgflpag : 18;
       unsigned pte$v_pgflx : 2;
     };
     struct {
-      unsigned page_bits : 9; //refer to pgtable.h for these
-      unsigned yet_unused : 1;
-      unsigned pte$v_typ0 : 1;
-      unsigned pte$v_typ1 : 1;
+      unsigned page_bits2 : 9; //refer to pgtable.h for these
+      unsigned yet_unused2 : 1;
+      unsigned pte$v_typ02 : 1;
+      unsigned pte$v_typ12 : 1;
       unsigned pte$v_gptx : 20;
     };
     struct {
-      unsigned page_bits : 9; //refer to pgtable.h for these
-      unsigned yet_unused : 1;
-      unsigned pte$v_typ0 : 1;
-      unsigned pte$v_typ1 : 1;
+      unsigned page_bits3 : 9; //refer to pgtable.h for these
+      unsigned yet_unused3 : 1;
+      unsigned pte$v_typ03 : 1;
+      unsigned pte$v_typ13 : 1;
       unsigned pte$v_pfn : 20;
     };
     struct {
-      unsigned page_bits : 9; //refer to pgtable.h for these
+      unsigned page_bits4 : 9; //refer to pgtable.h for these
       unsigned pte$v_window : 1;
-      unsigned pte$v_typ0 : 1;
-      unsigned pte$v_typ1 : 1;
-      unsigned pte$v_pfn : 20;
+      unsigned pte$v_typ04 : 1;
+      unsigned pte$v_typ14 : 1;
+      unsigned pte$v_pfn1 : 20;
     };
     struct {
       unsigned pte$v_valid : 1;
       unsigned nothing : 6;
       unsigned pte$v_global : 1;
       unsigned pte$v_gblwrt : 1;
-      unsigned pte$v_fill : 23;
+      unsigned pte$v_fill1 : 23;
     };
     unsigned long pte$l_all;
   };
